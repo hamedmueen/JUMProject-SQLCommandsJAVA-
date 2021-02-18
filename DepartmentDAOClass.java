8a56b1cad78e17c5166dd503a19fae91ed92b668
hamedmueen <64982261+hamedmueen@users.noreply.github.com>
2021-02-18 11:50:07 -0800

    Update DepartmentDAOClass.java

--- DepartmentDAOClass.java
+++ DepartmentDAOClass.java
@@ -1,5 +1,6 @@
 package com.cognixia.jumo.jdbc.connect;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -8,52 +9,66 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class CompanyDAOClass implements DAO{
+
+public class DepartmentDAOClass implements DAO {
+
+    Connection conn = ConnectionManager.getConnection();
+
     @Override
-    public boolean createTable() throws SQLException {
+    public boolean createTable() throws SQLException{
         Statement stmt = conn.createStatement();
-        boolean count = stmt.execute("CREATE TABLE company(" +
-                " company_id INT PRIMARY KEY NOT NULL, " +
-                " company_name VARCHAR(20) NOT NULL, " +
-                " headquarters VARCHAR(25) NOT NULL, " +
-                " industy VARCHAR(15) NOT NULL, " +
-                " global_strategy  VARCHAR(25) NOT NULL, " +
-                " organizational_culture VARCHAR(25) NOT NULL, " +
-                " revenue_in_millions INT NOT NULL)");
+        boolean count = stmt.execute("CREATE TABLE department(" +
+                " dept_id INT PRIMARY KEY NOT NULL, " +
+                " dept_name VARCHAR(20) NOT NULL, " +
+                " cell VARCHAR(15) NOT NULL, " +
+                " budget INT NOT NULL, " +
+                " FOREIGN KEY (company_id) REFERENCES company(company_id))");
         return count;
     }
+
+    @Override
+    public Department selectDepartment(int dept_id) {
+        Department dept = null;
+        try {
+            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM department WHERE dept_id = ?");
+            pstmt.setInt(1, dept_id);
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                Department d = new Department(rs.getInt("dept_id"), rs.getString("dept_name"), rs.getString("cell"), rs.getInt("budget"), rs.getInt("company_id"));
+                dept = d;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return dept;
+    }
+
 
     @Override
     public boolean insert() {
         int rows = 0;
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter company id: ");
+        System.out.print("Enter dept_id: ");
+        int dept_id = Integer.parseInt(in.nextLine());
+        System.out.println("Enter dept_name: ");
+        String dept_name = in.nextLine();
+        System.out.println("Enter cell: ");
+        String cell = in.nextLine();
+        System.out.println("Enter budget: ");
+        int budget = Integer.parseInt(in.nextLine());
+        System.out.println("Enter Company_id: ");
         int company_id = Integer.parseInt(in.nextLine());
-        System.out.println("Enter company name: ");
-        String company_name = in.nextLine();
-        System.out.println("Enter headquarters: ");
-        String headquarters = in.nextLine();
-        System.out.println("Enter industry: ");
-        String industry = in.nextLine();
-        System.out.println("Enter global strategy: ");
-        String global_strategy = in.nextLine();
-        System.out.println("Enter Organizational Culture: ");
-        String org_culture = in.nextLine();
-        System.out.println("Enter revenue_in_millions: ");
-        int revenue = Integer.parseInt(in.nextLine());
-        Company company = new Company(company_id,company_name,headquarters,industry, global_strategy,
-                org_culture, revenue);
+        Department department = new Department(dept_id,dept_name,cell,budget,company_id);
         try {
-            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO company (company_id, company_name, headquarters, "
-                    + " industry, global_strategy, organizational_culture, revenue_in_millions)"
-                    + " VALUES (?,?,?,?,?,?,?)");
-            pstmt.setInt(1, company.getCompany_id());
-            pstmt.setString(2, company.getCompany_name());
-            pstmt.setString(3,company.getHeadquarters());
-            pstmt.setString(4,company.getIndustry());
-            pstmt.setString(5, company.getGlobal_strategy());
-            pstmt.setString(6,company.getOrganizational_culture());
-            pstmt.setInt(7,company.getRevenue_in_millions());
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO department (dept_id, dept_name, cell, budget, company_id)"
+                    + " VALUES (?,?,?,?,?)");
+            pstmt.setInt(1, department.getDept_id());
+            pstmt.setString(2, department.getName());
+            pstmt.setString(3,department.getCell());
+            pstmt.setInt(4,department.getBudget());
+            pstmt.setInt(5,company_id);
             rows = pstmt.executeUpdate();
             pstmt.close();
         } catch (SQLException e) {
@@ -63,17 +78,18 @@ public class CompanyDAOClass implements DAO{
         in.close();
         return rows == 1;
 
-    };
+    }
 
     @Override
     public boolean delete() {
+        // TODO Auto-generated method stub
         int rows = 0;
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter company_id");
-        int company_id = Integer.parseInt(in.nextLine());
+        System.out.println("Enter dept_id");
+        int dept_id = Integer.parseInt(in.nextLine());
         try{
-            PreparedStatement pstmt = conn.prepareStatement("DELETE FROM company WHERE company_id = ?");
-            pstmt.setInt(2, company_id);
+            PreparedStatement pstmt = conn.prepareStatement("DELETE FROM department WHERE dept_id = ?");
+            pstmt.setInt(2,dept_id);
             rows = pstmt.executeUpdate();
             pstmt.close();
         }
@@ -86,65 +102,45 @@ public class CompanyDAOClass implements DAO{
 
     @Override
     public boolean update() {
+        // TODO Auto-generated method stub
         int rows = 0;
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter company_id: ");
-        int company_id = Integer.parseInt(in.nextLine());
+        System.out.println("Enter dept_id: ");
+        int dept_id = Integer.parseInt(in.nextLine());
         String stringUpdate = null;
         int intUpdate = 0;
         System.out.println("What do you want to update:");
         int select = Integer.parseInt(in.nextLine());
         try {
-            PreparedStatement pstmt = conn.prepareStatement("UPDATE company SET ? = ? WHERE company_id = ?");
+            PreparedStatement pstmt = conn.prepareStatement("UPDATE department SET ? = ? WHERE dept_id = ?");
             switch (select) {
                 case 1:
-                    System.out.println("Insert new company_id: ");
+                    System.out.println("Insert new dept_id: ");
                     intUpdate = Integer.parseInt(in.nextLine());
-                    pstmt.setString(1, "company_id");
+                    pstmt.setString(1, "dept_id");
                     pstmt.setInt(2, intUpdate);
-                    pstmt.setInt(3, company_id);
+                    pstmt.setInt(3, dept_id);
                     break;
                 case 2:
-                    System.out.println("Insert new company_name");
+                    System.out.println("Insert new dept_name");
                     stringUpdate = in.nextLine();
-                    pstmt.setString(1, "company_name");
+                    pstmt.setString(1, "dept_name");
                     pstmt.setString(2, stringUpdate);
-                    pstmt.setInt(3, company_id);
+                    pstmt.setInt(3, dept_id);
                     break;
                 case 3:
-                    System.out.println("Insert new headquarters");
+                    System.out.println("Insert new cell");
                     stringUpdate = in.nextLine();
-                    pstmt.setString(1, "headquarters");
+                    pstmt.setString(1, "cell");
                     pstmt.setString(2, stringUpdate);
-                    pstmt.setInt(3, company_id);
+                    pstmt.setInt(3, dept_id);
                     break;
                 case 4:
-                    System.out.println("Insert new industry");
-                    stringUpdate = in.nextLine();
-                    pstmt.setString(1, "industry");
-                    pstmt.setString(2, stringUpdate);
-                    pstmt.setInt(3, company_id);
-                    break;
-                case 5:
-                    System.out.println("Insert new global_strategy");
-                    stringUpdate = in.nextLine();
-                    pstmt.setString(1, "global_strategy");
-                    pstmt.setString(2, stringUpdate);
-                    pstmt.setInt(3, company_id);
-                    break;
-                case 6:
-                    System.out.println("Insert new organizational_culture");
-                    stringUpdate = in.nextLine();
-                    pstmt.setString(1, "organizational_culture");
-                    pstmt.setString(2, stringUpdate);
-                    pstmt.setInt(3, company_id);
-                    break;
-                case 7:
-                    System.out.println("Insert new revenue_in_millions");
+                    System.out.println("Insert new budget");
                     intUpdate = Integer.parseInt(in.nextLine());
-                    pstmt.setString(1, "revenue_in_millions");
+                    pstmt.setString(1, "budget");
                     pstmt.setInt(2, intUpdate);
-                    pstmt.setInt(3, company_id);
+                    pstmt.setInt(3, dept_id);
                     break;
                 default:
                     System.out.println("Invalid input");
