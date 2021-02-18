d4c726ad6d074be81b5229c15d0a3e17941d988e
hamedmueen <64982261+hamedmueen@users.noreply.github.com>
2021-02-18 07:54:34 -0800

    Add files via upload

---
+++ CompanyDAOClass.java
@@ -0,0 +1,26 @@
+package com.cognixia.jumo.jdbc.connect;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class CompanyDAOClass implements DAO{
+    @Override
+    public boolean createTable() throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean insert() {
+        return false;
+    }
+
+    @Override
+    public boolean delete() {
+        return false;
+    }
+
+    @Override
+    public boolean update() {
+        return false;
+    }
+}
