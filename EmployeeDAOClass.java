5f3eaa0d7cc7168d7bf9a44297eba981938b1874
hamedmueen <64982261+hamedmueen@users.noreply.github.com>
2021-02-18 11:50:57 -0800

    Update EmployeeDAOClass.java

--- EmployeeDAOClass.java
+++ EmployeeDAOClass.java
@@ -1,8 +1,7 @@
 package com.cognixia.jumo.jdbc.connect;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class EmployeeDAOClass implements DAO {
 
@@ -16,63 +15,50 @@ public class EmployeeDAOClass implements DAO {
                 " emp_name VARCHAR(40) NOT NULL," +
                 " job_title VARCHAR(20) NOT NULL," +
                 " salary INT NOT NULL," +
-                " dob date NOT NULL," +
+                " dob VARCHAR(10) NOT NULL," +
                 " FOREIGN KEY (dept_id) REFERENCES department(dept_id)," +
                 " FOREIGN KEY (address_id) REFERENCES address(address_id))");
         return count;
     }
 
     @Override
-    public List<Employee> selectAllFromEmployee() {
-        List<Employee> emps = new ArrayList<>();
+    public boolean insert() {
+        int rows = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter emp_id: ");
+        int emp_id = Integer.parseInt(in.nextLine());
+        System.out.println("Enter emp_name: ");
+        String emp_name = in.nextLine();
+        System.out.println("Enter job_title: ");
+        String job_title = in.nextLine();
+        System.out.println("Enter salary: ");
+        int salary = Integer.parseInt(in.nextLine());
+        System.out.println("Enter dob: ");
+        String dob = in.nextLine();
+        System.out.println("Enter dept_id: ");
+        int dept_id = Integer.parseInt(in.nextLine());
+        System.out.println("Enter address_id: ");
+        int address_id = Integer.parseInt(in.nextLine());
+        Employee employee = new Employee(emp_id,emp_name,job_title, salary, dob, dept_id,address_id);
         try {
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("Select * FROM employee");
-
-            while (rs.next()) {
-                Employee e = new Employee(rs.getInt("emp_id"),
-                        rs.getString("emp_name"),
-                        rs.getString("job_title"),
-                        rs.getInt("salary"),
-                        rs.getDate("dob"),
-                        rs.getInt("dept_id"),
-                        rs.getInt("address_id"));
-                emps.add(e);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return emps;
-    }
-
-    @Override
-    public Employee selectEmployee() {
-        Department department = null;
-        try{
-            PreparedStatement pstmt = conn.prepareStatement("SELECT * from employee where emp_id = ?");
-            //pstmt.setInt(1, id);
-            ResultSet rs = pstmt.executeQuery();
-            while(rs.next()){
-                department= new Department(rs.getInt("emp_id"),
-                        rs.getString("emp_name"),
-                        rs.getString("job_title"),
-                        rs.getInt("salary"));
-                        rs.getDate("dob"),
-                        rs.getInt("dept_id"),
-                        rs.getInt("address_id"));
-            }
-            rs.close();
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO employee (emp_id, company_name, headquarters, "
+                    + " industry, global_strategy, organizational_culture, revenue_in_millions)"
+                    + " VALUES (?,?,?,?,?,?,?)");
+            pstmt.setInt(1, employee.getEmpId());
+            pstmt.setString(2, employee.getName());
+            pstmt.setString(3, employee.getJobTitle());
+            pstmt.setInt(4, employee.getSalary());
+            pstmt.setString(5, employee.getDob());
+            pstmt.setInt(6,employee.getDeptId());
+            pstmt.setInt(7,employee.getAddId());
+            rows = pstmt.executeUpdate();
             pstmt.close();
         } catch (SQLException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        return null;
-    }
-
-    @Override
-    public boolean insert() {
-        return false;
+        in.close();
+        return rows == 1;
     }
 
     @Override
@@ -80,7 +66,7 @@ public class EmployeeDAOClass implements DAO {
         int rows = 0;
         Scanner in = new Scanner(System.in);
         System.out.println("Enter emp_id: ");
-        int address_id = Integer.parseInt(in.nextLine());
+        int emp_id = Integer.parseInt(in.nextLine());
         try{
             PreparedStatement pstmt = conn.prepareStatement("DELETE FROM employee WHERE emp_id = ?");
             pstmt.setInt(2,emp_id);
@@ -92,7 +78,6 @@ public class EmployeeDAOClass implements DAO {
         }
         in.close();
         return rows ==1;
-    }
     }
 
     @Override
@@ -133,27 +118,27 @@ public class EmployeeDAOClass implements DAO {
                 case 4:
                     System.out.println("Insert new salary");
                     intUpdate = Integer.parseInt(in.nextLine());
-                    pstmt.setInt(1, "salary");
+                    pstmt.setString(1, "salary");
                     pstmt.setString(2, stringUpdate);
                     pstmt.setInt(3, emp_id);
                     break;
                 case 5:
                     System.out.println("Insert new dob");
                     intUpdate = Integer.parseInt(in.nextLine());
-                    pstmt.setString(1, "zip");
+                    pstmt.setString(1, "dob");
                     pstmt.setString(2, stringUpdate);
                     pstmt.setInt(3, emp_id);
                     break;
                 case 6:
                     System.out.println("Insert new dept_id");
                     intUpdate = Integer.parseInt(in.nextLine());
-                    pstmt.setInt(1, "dept_id");
+                    pstmt.setString(1, "dept_id");
                     pstmt.setString(2, stringUpdate);
                     pstmt.setInt(3, emp_id);
                     break;
                 case 7:
-                    System.out.println("Insert new address_id)
-                    pstmt.setInt(1, "address_id");
+                    System.out.println("Insert new address_id");
+                    pstmt.setString(1, "address_id");
                     pstmt.setString(2, stringUpdate);
                     pstmt.setInt(3, emp_id);
                     break;
