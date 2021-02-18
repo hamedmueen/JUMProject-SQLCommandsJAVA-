baddde1df0ac89c090461d5356d1588331e8fcd7
hamedmueen <64982261+hamedmueen@users.noreply.github.com>
2021-02-18 07:43:50 -0800

    Update EmployeeDAOClass.java

--- EmployeeDAOClass.java
+++ EmployeeDAOClass.java
@@ -4,20 +4,39 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EmployeeDAOClass implements EmployeeDAO {
+public class EmployeeDAOClass implements DAO {
 
     Connection conn = ConnectionManager.getConnection();
 
+    @Override
+    public boolean createTable() throws SQLException {
+        Statement stmt = conn.createStatement();
+        boolean count = stmt.execute("CREATE TABLE employee(" +
+                "emp_id INT PRIMARY KEY NOT NULL," +
+                " emp_name VARCHAR(40) NOT NULL," +
+                " job_title VARCHAR(20) NOT NULL," +
+                " salary INT NOT NULL," +
+                " dob date NOT NULL," +
+                " FOREIGN KEY (dept_id) REFERENCES department(dept_id)," +
+                " FOREIGN KEY (address_id) REFERENCES address(address_id))");
+        return count;
+    }
 
     @Override
-    public List<Employee> getAllEmployees() {
+    public List<Employee> selectAllFromEmployee() {
         List<Employee> emps = new ArrayList<>();
         try {
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery("Select * FROM department");
 
             while (rs.next()) {
-                Employee e = new Employee(rs.getInt("emp_id"), rs.getString("emp_name"), rs.getString("job_title"),rs.getInt("salary"),rs.getDate("dob"),rs.getInt("dept_id"),rs.getInt("address_id"));
+                Employee e = new Employee(rs.getInt("emp_id"),
+                        rs.getString("emp_name"),
+                        rs.getString("job_title"),
+                        rs.getInt("salary"),
+                        rs.getDate("dob"),
+                        rs.getInt("dept_id"),
+                        rs.getInt("address_id"));
                 emps.add(e);
             }
 
@@ -28,35 +47,38 @@ public class EmployeeDAOClass implements EmployeeDAO {
     }
 
     @Override
-    public Department getEmployees(int id) {
+    public Employee selectEmployee() {
         Department department = null;
         try{
             PreparedStatement pstmt = conn.prepareStatement("SELECT * from address where emp_id = ?");
-            pstmt.setInt(1, id);
+            //pstmt.setInt(1, id);
             ResultSet rs = pstmt.executeQuery();
             while(rs.next()){
-                department= new Department(rs.getInt("dept_id"),rs.getString("dept_name"),rs.getString("cell"),rs.getInt("budget"));
+                department= new Department(rs.getInt("dept_id"),
+                        rs.getString("dept_name"),
+                        rs.getString("cell"),
+                        rs.getInt("budget"));
             }
             rs.close();
             pstmt.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return department;
+        return null;
     }
 
     @Override
-    public boolean addEmployees(Department department) {
+    public boolean insert() {
         return false;
     }
 
     @Override
-    public boolean deleteEmployees(int id) {
+    public boolean delete() {
         return false;
     }
 
     @Override
-    public boolean updateEmployees(Department department) {
+    public boolean update() {
         return false;
     }
 }
